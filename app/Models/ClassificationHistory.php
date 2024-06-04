32a565ca51367f19e58ffd57adc48a618fec3a56
virgo <virgostevanus27@gmail.com>
2024-06-04 13:54:54 +0700

    add model

---
+++ app/Models/ClassificationHistory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ClassificationHistory extends Model
+{
+    use HasFactory;
+
+    protected $table = 'classification_history';
+
+    protected $fillable = [
+        'user_id',
+        'fish_name',
+        'fish_type',
+        'fish_description',
+        'fish_food',
+        'fish_food_stall',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
