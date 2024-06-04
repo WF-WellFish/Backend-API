ccaaec7642295e098bccf1efef0c84a1aa6007e3
virgo <virgostevanus27@gmail.com>
2024-06-04 18:36:57 +0700

    test a unit test that interact with database

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Database\Factories\UserFactory;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -14,6 +16,13 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(400);
+        $response->assertStatus(200);
+    }
+
+    public function test_user_created_from_factory(): void
+    {
+        $user = User::factory()->create();
+
+        $this->assertDatabaseHas('users', ['username' => $user->username]);
     }
 }
