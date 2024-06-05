629eef3fe5fe8545ef547c386ac432f600816acb
virgo <virgostevanus27@gmail.com>
2024-06-06 03:15:27 +0700

    add test file for update profile actions and revised cloudbuid.yaml

---
+++ tests/Unit/Actions/Api/Profile/UpdateProfileActionTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit\Actions\Api\Profile;
+
+use App\Actions\Api\Profile\UpdateProfileAction;
+use PHPUnit\Framework\TestCase;
+
+class UpdateProfileActionTest extends TestCase
+{
+    // TODO: Add test cases
+    public function test_update_profile_action(): void
+    {
+        $this->assertTrue(true);
+    }
+}
