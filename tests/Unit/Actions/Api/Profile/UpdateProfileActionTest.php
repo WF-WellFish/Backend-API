244be5e77fdf6fda1c2809c1fdf1bd2e39786678
virgo <virgostevanus27@gmail.com>
2024-06-06 18:15:26 +0700

    add update profile action test

--- tests/Unit/Actions/Api/Profile/UpdateProfileActionTest.php
+++ tests/Unit/Actions/Api/Profile/UpdateProfileActionTest.php
@@ -2,13 +2,47 @@
 
 namespace Tests\Unit\Actions\Api\Profile;
 
-use PHPUnit\Framework\TestCase;
+use App\Actions\Api\Profile\UpdateProfileAction;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
 
 class UpdateProfileActionTest extends TestCase
 {
-    // TODO: Add test cases
+    /**
+     * Test that the update profile action works as expected
+     *
+     * @return void
+     */
     public function test_update_profile_action(): void
     {
-        $this->assertTrue(true);
+        $user = $this->createUser([
+            'name' => 'fake name'
+        ]);
+
+        $oldUser = $user->toArray();
+
+        Storage::fake('public');
+
+        $updatedUser = UpdateProfileAction::run($user, [
+            'name' => 'John Doe',
+            'profile_picture' => UploadedFile::fake()->image('profile.jpg')
+        ]);
+
+        // Assert that the profile picture has been uploaded
+        Storage::disk('public')->assertExists('profile_pictures/' . $updatedUser->profile_picture);
+
+        // Assert that the user has been updated
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id,
+            'name' => $updatedUser->name,
+            'profile_picture' => $updatedUser->profile_picture
+        ]);
+
+        // Assert that the old user data is not in the database
+        $this->assertDatabaseMissing('users', [
+            'id' => $user->id,
+            'name' => $oldUser['name']
+        ]);
     }
 }
